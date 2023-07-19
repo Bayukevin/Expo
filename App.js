ae43d0c7385fa243549f8f942f6d7edf2595bc5e
Bayu Kevin Farindra <bayukevinfarindra23@gmail.com>
2023-07-19 16:31:33 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
+import * as React from "react";
+import { View, Platform } from "react-native";
+import { WebView } from "react-native-webview";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return Platform.OS === "web" ? (
+      <iframe src="https://vin-qr.netlify.app/" height={'100%'} width={'100%'} />
+    ) : (
+      <View style={{ flex: 1 }}>
+        <StatusBar Style="dark" backgroundColor={"#000000"}/>
+        <WebView
+          source={{ uri: "https://vin-qr.netlify.app/" }}
+          style={{marginTop: 22, flex: 1}}
+        />
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
